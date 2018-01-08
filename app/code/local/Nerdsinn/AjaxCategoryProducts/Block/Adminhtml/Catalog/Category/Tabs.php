220753f493aa53f109d98e2e9f4219b20eb97510
thenerdsinn <34534244+thenerdsinn@users.noreply.github.com>
2018-01-08 15:32:46 +0500

    Initial Commit

---
+++ app/code/local/Nerdsinn/AjaxCategoryProducts/Block/Adminhtml/Catalog/Category/Tabs.php
@@ -0,0 +1,5 @@
+<?php
+class Nerdsinn_AjaxCategoryProducts_Block_Adminhtml_Catalog_Category_Tabs extends Mage_Adminhtml_Block_Catalog_Category_Tabs
+{
+}
+			
---
+++ app/code/local/Nerdsinn/AjaxCategoryProducts/Helper/Data.php
@@ -0,0 +1,5 @@
+<?php
+class Nerdsinn_AjaxCategoryProducts_Helper_Data extends Mage_Core_Helper_Abstract
+{
+}
+	 
